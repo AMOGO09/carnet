b1d165263a5c218474fe90efef44315e18157815
AMOGO09 <84076612+AMOGO09@users.noreply.github.com>
2021-05-14 11:49:50 +0200

    Add files via upload

---
+++ infos.php
@@ -0,0 +1,65 @@
+<?php
+	$host='localhost';
+	$port= 3306;
+	$dbname='carnet';
+	$user= 'root';
+	$pwd='';
+//connexion
+	try{
+		//$db=new PDO("mysql:host=$host;port=$port;dbname=$dbname",$user,$pwd,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+		$db=new PDO("mysql:host=$host;port=$port;dbname=$dbname",$user,$pwd,array(PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC));
+		echo "   ";
+	}
+
+	catch(PDOException $e){
+
+		die('Erreur :'.$e->getMessage());
+	}
+
+	$pdoStat = $db->prepare('SELECT * FROM contact');
+
+	$executeIsok = $pdoStat->execute();
+
+
+	$contacts = $pdoStat->fetchAll();
+
+
+	?>
+
+	<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+
+
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="contact.css">
+    </head>
+
+<body>
+        <div><h1>Informations !!</h1>
+<ul>
+    <li>
+    	 <?php
+   	foreach ($contacts as $contact);
+
+   	var_dump($contacts);
+?>
+
+		<li>
+	<?. $contact['nom']?> <?. $contact['prenom']?> <?. $contact['telephone']?> <?. $contact['adresse'] ?> 
+	<a class="" href="contact.php"> Retour au formulaire.</a>
+</li>
+
+    </li>
+    
+</ul>
+        </div>
+    </body>
+       
